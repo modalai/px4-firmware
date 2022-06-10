e31d00739e6d6e5ede40235b03ee5a9250d178db
Your Name <you@example.com>
2022-06-10 12:26:05 -0500

    update to code base for testing

--- boards/modalai/rb5-flight/test/client.py
+++ boards/modalai/rb5-flight/test/client.py
@@ -1,19 +1,49 @@
 import socket
+import serial
+import json
+import argparse
 import time
-pings = 1
-#Send ping 10 times
-while pings < 11:
 
-    #Create a UDP socket
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+from pymavlink import mavutil
+from pymavlink.dialects.v20 import ardupilotmega as mavlink2
 
-    #Set a timeout value of 1 second
-    clientSocket.settimeout(1)
+class clientSocket:
 
-    #Ping to server
-    message = 'test'.encode('utf-8')
+    def __init__(self, port):
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_addr = ("", port)
 
-    addr = ("127.0.0.1", 8000)
-    clientSocket.sendto(message, addr)
-    pings+=1
-    time.sleep(1)
+    def send_msgs(self, message):
+        try:
+            self.clientSocket.sendto(message.encode('utf-8'), self.server_addr)
+        except socket.error as e:
+            pass
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Send data to serial port')
+    parser.add_argument('--server_port', '-s', type=int, required=True)
+    parser.add_argument('--rc_channel_json', '-rc', type=bool, required=False)
+    parser.add_argument('--radio_status_json', '-rs', type=bool, required=False)
+    parser.add_argument('--parameter_name', '-pn', type=str, required=False)
+    parser.add_argument('--parameter_value', '-pv', type=str, required=False)
+
+    args = parser.parse_args()
+
+    client = clientSocket(args.server_port)
+
+    data_dict ={}
+    if(args.rc_channel_json or args.radio_status_json):
+        if(args.rc_channel_json):
+            data_dict["message_type"] = "rc_channel"
+            data_dict["param_name"] = str(args.parameter_name)
+            data_dict["parameter_value"] = str(args.parameter_value)
+        elif(args.radio_status_json):
+            data_dict["message_type"] = "radio_status"
+            data_dict["param_name"] = str(args.parameter_name)
+            data_dict["parameter_value"] = str(args.parameter_value)
+
+        client.send_msgs(json.dumps(data_dict))
+
+if __name__ == "__main__":
+    main()
--- boards/modalai/rb5-flight/test/server.py
+++ boards/modalai/rb5-flight/test/server.py
@@ -3,6 +3,7 @@ import serial
 import json
 import argparse
 import time
+
 from pymavlink import mavutil
 from pymavlink.dialects.v20 import ardupilotmega as mavlink2
 
@@ -44,22 +45,18 @@ class fifo(object):
     def read(self):
         return self.buf.pop(0)
 
-def encode_rc_channel():
+def encode_rc_channel(json_data):
 	f = fifo()
-	rc_channel_values = [65535 for _ in range(18)]
 	mavlink_message = mavlink2.MAVLink(f)
-	m = mavlink_message.rc_channels_override_encode(
-		1,  # target_system
-		1,  # target_component
-		*rc_channel_values)
+	m = mavlink_message.rc_channels_override_encode(**json_data)
 	m.pack(mavlink_message)
 	encoded_buffer = m.get_msgbuf()
 	return encoded_buffer
 
-def encode_radio_status():
+def encode_radio_status(json_data):
 	f = fifo()
 	mavlink_message = mavlink2.MAVLink(f)
-	m = mavlink_message.radio_status_encode(rssi, remrssi, txbuf, noise, remnoise, rxerrors, fixed)
+	m = mavlink_message.radio_status_encode(**json_data)
 	m.pack(mavlink_message)
 	encoded_buffer = m.get_msgbuf()
 	return encoded_buffer
@@ -69,17 +66,46 @@ def main():
 	parser.add_argument('--server_port', '-s', type=int, required=True)
 	parser.add_argument('--serial_port', '-p', type=str, required=True)
 	parser.add_argument('--serial_baudrate', '-b', type=int, required=True)
+	parser.add_argument('--rc_channel_json', '-rc', type=str, required=False)
+	parser.add_argument('--radio_status_json', '-rs', type=str, required=False)
 
 	args = parser.parse_args()
+
+	if(args.rc_channel_json):
+		rc_channel_json = json.load(args.rc_channel_json)
+		encoded_rc_json = encode_rc_channel(rc_channel_json['mavlink_message'])
+
+	if(args.radio_status_json):
+		radio_status_json = json.load(args.radio_status_json)
+		encoded_radio_status_json = encode_radio_status(radio_status_json['mavlink_message'])
 
 	writer = serialWrite(args.serial_port, args.serial_baudrate)
 	server = serverSocket(args.server_port)
 
 	while True:
 		data = server.receive_msgs()
-		if(data):
-			print(data)
-		time.sleep(1)
+		try:
+			json_data = json.loads(data)
+			if(json_data):
+				if(json_data['message_type'] == 'rc_channel'):
+					encoded_rc_json = encode_rc_channel(json_data)
+					writer.write(encoded_rc_channel_data)
+
+				if(json_data['message_type'] == 'radio_status'):
+					encoded_radio_status_json = encode_radio_status(json_data)
+					writer.write(encoded_radio_status_data)
+
+			if(encoded_radio_status_json or encoded_rc_json):
+				if(encoded_radio_status_json):
+					writer.write(encoded_radio_status_data)
+
+				if(encoded_rc_json):
+					writer.write(encoded_rc_json)
+
+				pass
+
+		except Exception as e:
+			pass
 
 if __name__ == "__main__":
 	main()
