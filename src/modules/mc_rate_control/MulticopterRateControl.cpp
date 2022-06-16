effc07add42d0db121cc05ab06b7a80a4434903b
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-06-16 16:05:36 -0400

    Got rid of conservative landing integral check.

--- src/modules/mc_rate_control/MulticopterRateControl.cpp
+++ src/modules/mc_rate_control/MulticopterRateControl.cpp
@@ -219,7 +219,7 @@ MulticopterRateControl::Run()
 			}
 
 			// run rate controller
-			const Vector3f att_control = _rate_control.update(rates, _rates_sp, angular_accel, dt, _maybe_landed || _landed);
+			const Vector3f att_control = _rate_control.update(rates, _rates_sp, angular_accel, dt, _landed);
 
 			// publish rate controller status
 			rate_ctrl_status_s rate_ctrl_status{};
