89019eb84da1f617a20b7e3b0034aa4000f82019
Eric Katzfey <eric.katzfey@modalai.com>
2022-08-01 21:34:42 -0700

    Added more recv task functionality

--- src/drivers/modalai/mavlink_bridge/mavlink_bridge.cpp
+++ src/drivers/modalai/mavlink_bridge/mavlink_bridge.cpp
@@ -48,17 +48,71 @@ extern "C" { __EXPORT int mavlink_bridge_main(int argc, char *argv[]); }
 namespace mavlink_bridge
 {
 
-static bool _is_running = false;
-volatile bool _task_should_exit = false;
-static px4_task_t _task_handle = -1;
+#define UDP_READ_BUF_LEN (32*1024)
+
 static int sockfd_vvpx4;
 static struct sockaddr_in vvpx4_addr;
+
 bool debug = false;
 
 int start(int argc, char *argv[]);
 int stop();
 int status();
 void usage();
+
+void recv_task(int argc, char *argv[]) {
+	int i, bytes_read, msg_received;
+	char buf[UDP_READ_BUF_LEN];
+	struct sockaddr_in si_other;
+	socklen_t slen = sizeof(si_other);
+	mavlink_message_t msg;
+	mavlink_status_t status;
+
+	if (debug) PX4_INFO("Recv task starting");
+
+	while (true) {
+		// Receive UDP message from voxl-vision-px4, this is blocking with timeout
+		bytes_read = recvfrom(sockfd_vvpx4, buf, UDP_READ_BUF_LEN, MSG_WAITALL,
+							  (struct sockaddr*) &si_other, &slen);
+
+		if (bytes_read < 0) {
+ 			if (errno != EAGAIN) PX4_ERR("ERROR: UDP recvfrom had a problem");
+			// EAGAIN error means the timeout worked
+			else if (debug) PX4_INFO("Recv task udp timeout");
+		} else if (bytes_read > 0) {
+			msg.msgid = msg.sysid = 0;
+			for (i = 0; i < bytes_read; i++) {
+				msg_received = mavlink_parse_char(0, buf[i], &msg, &status);
+
+				// check for dropped packets
+				// TODO CHECK THIS, maybe bigger read buffer?
+				if (status.packet_rx_drop_count != 0) {
+					if (debug) PX4_INFO("WARNING: UDP listener dropped %d packets", status.packet_rx_drop_count);
+				}
+
+				// msg_received indicates this byte was the end of a complete packet
+				if (msg_received) {
+					if (debug) {
+						PX4_INFO("UDP recv msg ID: %d sysid:%d from port:%d IP: %s", \
+								 msg.msgid, msg.sysid, ntohs(si_other.sin_port), \
+								 inet_ntoa(si_other.sin_addr));
+
+						if (i != bytes_read - 1) {
+							PX4_INFO("Warning: more than one Mavlink packet in UDP packet. %d %d", i, bytes_read);
+							PX4_INFO("         Suggest setting udp_mtu to 0 in voxl-vision-px4");
+						}
+					}
+				}
+
+				if (( ! msg_received) && (i == bytes_read - 1) && (debug)) {
+					PX4_INFO("Detected unused bytes at end of UDP packet");
+				}
+			}
+		} else if (debug) PX4_INFO("recvfrom returned 0");
+	}
+
+	if (debug) PX4_INFO("Recv task ending");
+}
 
 void task_main(int argc, char *argv[]) {
 	int ch;
@@ -75,54 +129,63 @@ void task_main(int argc, char *argv[]) {
 		}
 	}
 
-	_is_running = true;
+	if (debug) PX4_INFO("Main task starting");
+
+	int ret = px4_task_spawn_cmd("mavlink_bridge_recv",
+								 SCHED_DEFAULT,
+								 SCHED_PRIORITY_DEFAULT,
+								 2000,
+								 (px4_main_t) &recv_task,
+								 (char *const *) argv);
+	if (ret < 0) {
+		PX4_ERR("Receive task start failed");
+		return;
+	}
 
     int mavlink_rx_msg_fd = orb_subscribe(ORB_ID(mavlink_rx_msg));
-
 	struct mavlink_msg_s incoming_msg;
-
     px4_pollfd_struct_t fds[1] = { { .fd = mavlink_rx_msg_fd,  .events = POLLIN } };
 
-	while ( ! _task_should_exit) {
+	while (true) {
     	px4_poll(fds, 2, 1000);
     	if (fds[0].revents & POLLIN) {
     		orb_copy(ORB_ID(mavlink_rx_msg), mavlink_rx_msg_fd, &incoming_msg);
-			PX4_INFO("Got incoming mavlink msg of length %u", incoming_msg.msg_len);
+			if (debug) PX4_INFO("Got incoming mavlink msg of length %u", incoming_msg.msg_len);
 			sendto(sockfd_vvpx4, incoming_msg.msg, incoming_msg.msg_len,
 			       MSG_CONFIRM, (const struct sockaddr*) &vvpx4_addr, sizeof(vvpx4_addr));
 		}
-		usleep(500);
 	}
+
+	if (debug) PX4_INFO("Main task ending");
 }
 
 int start(int argc, char *argv[])
 {
-	if (_is_running) {
-		PX4_WARN("already running");
-		return -1;
-	}
-
 	sockfd_vvpx4 = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd_vvpx4 < 0) {
 		PX4_ERR("ERROR: UDP socket creation failed");
 		return -1;
 	}
 
+	// set receive timeout for the socket
+	struct timeval tv;
+	tv.tv_sec = 0;
+	tv.tv_usec = 500000;
+	setsockopt(sockfd_vvpx4, SOL_SOCKET, SO_RCVTIMEO, (const char*) &tv, sizeof tv);
+
 	// Address of the QGC port in voxl-vision-px4
 	vvpx4_addr.sin_family = AF_INET;
 	vvpx4_addr.sin_port = htons(14550);
 	vvpx4_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-	_task_should_exit = false;
+	int ret = px4_task_spawn_cmd("mavlink_bridge_main",
+								 SCHED_DEFAULT,
+								 SCHED_PRIORITY_DEFAULT,
+								 2000,
+								 (px4_main_t) &task_main,
+								 (char *const *) argv);
 
-	_task_handle = px4_task_spawn_cmd("mavlink_bridge_main",
-					  SCHED_DEFAULT,
-					  SCHED_PRIORITY_DEFAULT,
-					  2000,
-					  (px4_main_t)&task_main,
-					  (char *const *)argv);
-
-	if (_task_handle < 0) {
+	if (ret < 0) {
 		PX4_ERR("task start failed");
 		return -1;
 	}
@@ -130,31 +193,8 @@ int start(int argc, char *argv[])
 	return 0;
 }
 
-int stop()
-{
-	if (!_is_running) {
-		PX4_WARN("not running");
-		return -1;
-	}
-
-	_task_should_exit = true;
-
-	while (_is_running) {
-		usleep(200000);
-		PX4_INFO(".");
-	}
-
-	_task_handle = -1;
-	return 0;
-}
-
-int status() {
-	PX4_INFO("running: %s", _is_running ? "yes" : "no");
-	return 0;
-}
-
 void usage() {
-	PX4_INFO("Usage: mavlink_bridge {start|info|stop}");
+	PX4_INFO("Usage: mavlink_bridge start");
 }
 
 }
@@ -171,10 +211,6 @@ int mavlink_bridge_main(int argc, char *argv[]) {
 
 	if (!strcmp(verb, "start")) {
 		return mavlink_bridge::start(argc - 1, argv + 1);
-	} else if (!strcmp(verb, "stop")) {
-		return mavlink_bridge::stop();
-	} else if (!strcmp(verb, "status")) {
-		return mavlink_bridge::status();
 	} else {
 		mavlink_bridge::usage();
 		return -1;
