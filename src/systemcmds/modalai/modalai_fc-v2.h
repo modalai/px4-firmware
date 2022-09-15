f40526e0de8df6c9001aa4291fe5a3b2e0b2daf3
modaljc <jacob.camarillo@modalai.com>
2022-09-15 13:14:09 -0700

    Added test for J14 connector: external SPI

--- src/systemcmds/modalai/modalai_fc-v2.h
+++ src/systemcmds/modalai/modalai_fc-v2.h
@@ -156,6 +156,23 @@ typedef enum {
 #define M0087_J12_PIN_2_OUT             _MK_GPIO_OUTPUT(GPIO_PORTC|GPIO_PIN6)
 #define M0087_J12_PIN_3_IN              _MK_GPIO_INPUT(GPIO_PORTC|GPIO_PIN7)
 
+//
+// AVIATOR SPI/I2C/ADC EXPANSION CONNECTOR
+//   M0087- J14
+//             PC6 PIN2 - out
+//             PC7 PIN3 - in
+//
+#define  M0087_J14_PIN_2_OUT				_MK_GPIO_OUTPUT(GPIO_PORTA|GPIO_PIN6)
+#define  M0087_J14_PIN_3_OUT				_MK_GPIO_OUTPUT(GPIO_PORTG|GPIO_PIN14)
+#define  M0087_J14_PIN_4_OUT				_MK_GPIO_OUTPUT(GPIO_PORTB|GPIO_PIN3)
+#define  M0087_J14_PIN_5_OUT				_MK_GPIO_OUTPUT(GPIO_PORTI|GPIO_PIN10)
+#define  M0087_J14_PIN_6_OUT				_MK_GPIO_OUTPUT(GPIO_PORTF|GPIO_PIN0)
+#define  M0087_J14_PIN_7_IN				_MK_GPIO_INPUT(GPIO_PORTF|GPIO_PIN1)
+#define  M0087_J14_PIN_8_IN				_MK_GPIO_INPUT(GPIO_PORTF|GPIO_PIN12)
+#define  M0087_J14_PIN_9_IN				_MK_GPIO_INPUT(GPIO_PORTB|GPIO_PIN0)
+#define  M0087_J14_PIN_10_IN				_MK_GPIO_INPUT(GPIO_PORTA|GPIO_PIN0)
+#define  M0087_J14_PIN_11_IN				_MK_GPIO_INPUT(GPIO_PORTA|GPIO_PIN4)
+
 void	modalai_print_usage_v2(void);
 void	modalai_print_usage_con_gpio_test_v2(void);
 int 	modalai_con_gpio_test_v2(uint8_t con, uint8_t pin, bool state);
