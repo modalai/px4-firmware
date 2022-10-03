4599696472e7728d364cc7929ffabdec59f6d756
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-10-03 10:48:22 -0400

    Initial commit of (non-functional) module to switch between sets of parameters.

---
+++ src/modules/param_set_selector/param_set_selector.cpp
@@ -0,0 +1,191 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2012-2022 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file sensors.cpp
+ *
+ * @author Daniel M. Sahu <>
+ */
+
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/getopt.h>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/tasks.h>
+#include <px4_platform_common/time.h>
+#include <px4_platform_common/log.h>
+#include <lib/parameters/param.h>
+#include <uORB/topics/parameter_update.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+
+using namespace time_literals;
+
+class ParamSetSelector : public ModuleBase<ParamSetSelector>, public ModuleParams, public px4::ScheduledWorkItem
+{
+public:
+	ParamSetSelector();
+	~ParamSetSelector() override;
+
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
+	bool init();
+
+private:
+	void Run() override;
+
+	/**< notification of parameter updates */
+	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
+
+	/**< loop performance counter */
+	perf_counter_t	_loop_perf;
+
+	/**
+	 * Check for changes in parameters.
+	 */
+	void parameter_update_poll(bool forced = false);
+};
+
+ParamSetSelector::ParamSetSelector() :
+	ModuleParams(nullptr),
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::lp_default),
+	_loop_perf(perf_alloc(PC_ELAPSED, MODULE_NAME))
+{
+	updateParams();
+}
+
+ParamSetSelector::~ParamSetSelector()
+{
+	ScheduleClear();
+}
+
+void ParamSetSelector::parameter_update_poll(bool forced)
+{
+	// check for parameter updates
+	if (_parameter_update_sub.updated() || forced) {
+		// clear update
+		parameter_update_s pupdate;
+		_parameter_update_sub.copy(&pupdate);
+
+		// update parameters from storage
+		updateParams();
+	}
+}
+
+void ParamSetSelector::Run()
+{
+	if (should_exit()) {
+		exit_and_cleanup();
+		return;
+	}
+
+	// begin perf counter
+	perf_begin(_loop_perf);
+
+	/* check parameters for updates */
+	parameter_update_poll();
+
+	// end perf counter
+	perf_end(_loop_perf);
+}
+
+int ParamSetSelector::task_spawn(int argc, char *argv[])
+{
+	ParamSetSelector *instance = new ParamSetSelector();
+
+	if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+		if (instance->init()) {
+			return PX4_OK;
+		}
+
+	} else {
+		PX4_ERR("alloc failed");
+	}
+
+	delete instance;
+	_object.store(nullptr);
+	_task_id = -1;
+
+	return PX4_ERROR;
+}
+
+bool ParamSetSelector::init()
+{
+	return true;
+}
+
+int ParamSetSelector::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int ParamSetSelector::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+
+This module toggles between disjoint collections (sets) of parameters.
+
+### Implementation
+
+Monitors the 'PARAM_SET' parameter. If the parameter is updated, switch
+to the set defined by that parameter and update all corresponding parameters.
+
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_NAME("param_set_selector", "system");
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+	return 0;
+}
+
+extern "C" __EXPORT int param_set_selector_main(int argc, char *argv[])
+{
+	return ParamSetSelector::main(argc, argv);
+}
