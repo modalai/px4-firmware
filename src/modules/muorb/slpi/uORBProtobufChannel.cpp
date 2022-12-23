6e1eb25f1548273e3d8acbfe2d3f56f7557b29e3
Eric Katzfey <eric.katzfey@modalai.com>
2022-12-22 19:22:25 -0800

    Some work on threads, priorities, and work queues

--- src/modules/muorb/slpi/uORBProtobufChannel.cpp
+++ src/modules/muorb/slpi/uORBProtobufChannel.cpp
@@ -70,7 +70,7 @@ qurt_thread_t aggregator_tid;
 qurt_thread_attr_t aggregator_attr;
 // 1 is highest priority, 255 is lowest. Set it low.
 const uint32_t aggregator_thread_priority = 240;
-const uint32_t aggregator_stack_size = 8096;
+const uint32_t aggregator_stack_size = 8192;
 char aggregator_stack[aggregator_stack_size];
 
 static void aggregator_thread_func(void *ptr)
@@ -288,6 +288,7 @@ int px4muorb_orb_initialize(fc_func_ptrs *func_ptrs, int32_t clock_offset_us)
 		// Initialize the interrupt callback registration
 		register_interrupt_callback_initalizer(muorb_func_ptrs.register_interrupt_callback);
 
+		work_queues_init();
 		hrt_work_queue_init();
 
 		const char *argv[3] = { "slpi", "start" };
@@ -304,6 +305,9 @@ int px4muorb_orb_initialize(fc_func_ptrs *func_ptrs, int32_t clock_offset_us)
 		qurt_thread_attr_init(&aggregator_attr);
 		qurt_thread_attr_set_stack_addr(&aggregator_attr, aggregator_stack);
 		qurt_thread_attr_set_stack_size(&aggregator_attr, aggregator_stack_size);
+		char thread_name[QURT_THREAD_ATTR_NAME_MAXLEN];
+ 		strncpy(thread_name, "PX4_muorb_agg", QURT_THREAD_ATTR_NAME_MAXLEN);
+		qurt_thread_attr_set_name(&aggregator_attr, thread_name);
 		qurt_thread_attr_set_priority(&aggregator_attr, aggregator_thread_priority);
 		(void) qurt_thread_create(&aggregator_tid, &aggregator_attr, aggregator_thread_func, NULL);
 
