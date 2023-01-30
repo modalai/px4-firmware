618909cbe45097f63dc68cd5d01f4542dc3ef863
Eric Katzfey <eric.katzfey@modalai.com>
2023-01-30 12:39:17 -0800

    Merged in 8afbc16280aa4a521da9f68df31b102e333b4eb3

--- src/modules/mc_pos_control/MulticopterPositionControl.cpp
+++ src/modules/mc_pos_control/MulticopterPositionControl.cpp
@@ -524,15 +524,16 @@ void MulticopterPositionControl::Run()
 
 			// update states
 			if (!PX4_ISFINITE(_setpoint.position[2])
-			    && PX4_ISFINITE(_setpoint.velocity[2]) && (fabsf(_setpoint.velocity[2]) > FLT_EPSILON)
+			    && PX4_ISFINITE(_setpoint.velocity[2])
 			    && PX4_ISFINITE(vehicle_local_position.z_deriv) && vehicle_local_position.z_valid && vehicle_local_position.v_z_valid) {
 				// A change in velocity is demanded and the altitude is not controlled.
 				// Set velocity to the derivative of position
 				// because it has less bias but blend it in across the landing speed range
 				//  <  MPC_LAND_SPEED: ramp up using altitude derivative without a step
 				//  >= MPC_LAND_SPEED: use altitude derivative
-				float weighting = fminf(fabsf(_setpoint.velocity[2]) / _param_mpc_land_speed.get(), 1.f);
-				states.velocity(2) = vehicle_local_position.z_deriv * weighting + vehicle_local_position.vz * (1.f - weighting);
+				// float weighting = fminf(fabsf(_setpoint.velocity[2]) / _param_mpc_land_speed.get(), 1.f);
+				// states.velocity(2) = vehicle_local_position.z_deriv * weighting + vehicle_local_position.vz * (1.f - weighting);
+				states.velocity(2) = vehicle_local_position.z_deriv;
 			}
 
 			_control.setState(states);
