6fdb6efb148202a68691707cc7164b728b9b5a5b
Eric Katzfey <eric.katzfey@modalai.com>
2025-08-29 09:22:36 -0700

    Added git submodule commands to build-deps.sh

--- boards/modalai/voxl2/scripts/build-deps.sh
+++ boards/modalai/voxl2/scripts/build-deps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git submodule init boards/modalai/voxl2/libfc-sensor-api
+git submodule update boards/modalai/voxl2/libfc-sensor-api
+
 cd boards/modalai/voxl2/libfc-sensor-api
 rm -fR build
 mkdir build
@@ -7,3 +10,4 @@ cd build
 CC=/home/4.1.0.4/tools/linaro64/bin/aarch64-linux-gnu-gcc cmake ..
 make
 cd ../../../../..
+
