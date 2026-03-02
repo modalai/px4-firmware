c9a3b814cf47effd4c7a30f91e48c8a27fbd853c
Eric Katzfey <eric.katzfey@modalai.com>
2026-03-02 16:33:08 -0700

    Port vfc_logger module and VfcStatus message from voxl-fpv-dev
    Add VFC (Vision Flight Controller) logger module that receives VFC
    status data over MPA pipe and publishes it as a vfc_status uORB topic.
    Includes message definition, build integration, startup in both normal
    and HITL scripts, and logging of the new topic.

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -178,6 +178,8 @@ void LoggedTopics::add_default_topics()
 	add_topic_multi("timesync_status", 1000, 3);
 	add_topic_multi("telemetry_status", 1000, 4);
 
+	add_topic("vfc_status");
+
 	// EKF multi topics
 	{
 		// optionally log all estimator* topics at minimal rate
