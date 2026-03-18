5a942866bb09f39732e27e6b3beb7d571c68b8f8
Eric Katzfey <eric.katzfey@modalai.com>
2026-03-18 09:43:50 -0700

    Added i2c uninitialize on probe failure to free up port

--- src/lib/drivers/device/qurt/I2C.cpp
+++ src/lib/drivers/device/qurt/I2C.cpp
@@ -48,6 +48,10 @@
 #include <px4_platform_common/px4_config.h>
 #include <px4_platform_common/i2c_spi_buses.h>
 
+extern "C" {
+	__EXPORT void fc_uninitialize_i2c_bus(int fd);
+}
+
 namespace device
 {
 
@@ -120,7 +124,7 @@ I2C::init()
 	_i2c_fd = _config_i2c_bus(get_device_bus(), get_device_address(), _frequency);
 	pthread_mutex_unlock(_mutex);
 
-	if (_i2c_fd == PX4_ERROR) {
+	if (_i2c_fd == -1) {
 		PX4_ERR("i2c init failed");
 		goto out;
 	}
@@ -130,6 +134,8 @@ I2C::init()
 
 	if (ret != OK) {
 		PX4_ERR("i2c probe failed");
+		fc_uninitialize_i2c_bus(_i2c_fd);
+		_i2c_fd = -1;
 		goto out;
 	}
 
